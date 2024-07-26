3623f304accd0b88078201c835a15c00bca0684e
DanilVereshchagin <diafin.bernuly@mail.ru>
2024-07-26 13:44:00 +0300

    fix navbar 2

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,29 @@
 function loadNavbar() {
 	const box = document.getElementById('navbar_content');
 
+	let wait = 0;
+
 	fetch('./navbar.html')
 		.then(response => response.text())
 		.then(html => {
 			box.innerHTML = html;
+			wait = 1;
 		})
 		.catch(error => {
 			console.log(error);
-
-			fetch('../navbar.html')
-				.then(response => response.text())
-				.then(html => {
-					box.innerHTML = html;
-				})
-				.catch(error => {
-					console.log(error);
-				});
 		});
+
+	if (wait != 1) {
+		fetch('../navbar.html')
+			.then(response => response.text())
+			.then(html => {
+				box.innerHTML = html;
+				wait = 1;
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 }
 
 loadNavbar();
