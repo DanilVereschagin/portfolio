5f3b9a41b6e01c17699b577bea953be8b094a134
DanilVereshchagin <diafin.bernuly@mail.ru>
2024-07-26 14:09:13 +0300

    fix navbar 6

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 function loadNavbar() {
 	const box = document.getElementById('navbar_content');
 
-	fetch('./navbar/navbar.js')
+	fetch('./navbar/navbar.html')
 		.then(response => response.text())
 		.then(html => {
 			box.innerHTML = html;
