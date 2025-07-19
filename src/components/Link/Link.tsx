50bc31533ec97ad5eb29fb0286044fd7a94da0d5
Danil Vereshchagin <diafin.bernuly@mail.ru>
2025-07-20 00:04:48 +0300

    Fix bug and add select of type skills  sorting

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 interface Props {
-	url?: string;
+	url: string;
 	title?: string;
 }
 
 export const Link: React.FC<Props> = ({ url, title }) => {
 	return (
-		<a
-			href={url}
+		<NavLink
+			to={url}
 			className='p-2 text-lg border border-fuchsia-400 rounded-xl 
 			
 			 bg-white text-black shadow-xl/30 shadow-purple-400 
 			 hover:bg-transparent hover:text-white hover:shadow-none'
 		>
 			{title}
-		</a>
+		</NavLink>
 	);
 };
--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,8 +1,45 @@
+import { useState } from 'react';
+import { cn } from '../../shared/lib/utils';
+
 const Skills = () => {
+	const [activeTab, setActiveTab] = useState('level');
+
 	return (
-		<>
-			<h1 className='text-2xl text-amber-300'>Навыки</h1>
-		</>
+		<div className='flex flex-col gap-4 bg-transparent'>
+			<div className='flex gap-4 bg-transparent text-lg'>
+				<button
+					type='submit'
+					className={cn(
+						'border-b border-white/45 text-white p-2 hover:shadow-lg/30 hover:shadow-purple-400 hover:bg-transparent',
+						{
+							'shadow-lg/30 shadow-purple-400 bg-transparent':
+								activeTab === 'level',
+						}
+					)}
+					onClick={() => {
+						setActiveTab('level');
+					}}
+				>
+					Уровень навыков
+				</button>
+				<button
+					type='submit'
+					className={cn(
+						'border-b border-white/45 text-white p-2 hover:shadow-lg/30 hover:shadow-purple-400 hover:bg-transparent',
+						{
+							'shadow-lg/30 shadow-purple-400 hover:bg-transparent':
+								activeTab === 'category',
+						}
+					)}
+					onClick={() => {
+						setActiveTab('category');
+					}}
+				>
+					Категории
+				</button>
+			</div>
+			<div></div>
+		</div>
 	);
 };
 
