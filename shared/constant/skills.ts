b6b1713bdd1ebb2068789f07ca878b79b3d78bb5
Danil Vereshchagin <diafin.bernuly@mail.ru>
2025-07-22 20:56:10 +0300

    Add skills

---
+++ shared/constant/skills.ts
@@ -0,0 +1,79 @@
+export const skills = [
+	{
+		id: 1,
+		title: 'React',
+		level: 'average',
+		icon: 'logo/react.png',
+		type: 'frontend',
+	},
+	{
+		id: 2,
+		title: 'Next.js',
+		level: 'average',
+		icon: 'logo/nextjs.png',
+		type: 'frontend',
+	},
+	{
+		id: 7,
+		title: 'PostgreSQL',
+		level: 'average',
+		icon: 'logo/postgresql.png',
+		type: 'database',
+	},
+
+	{
+		id: 6,
+		title: 'MySQL',
+		level: 'average',
+		icon: 'logo/mysql.png',
+		type: 'database',
+	},
+	{
+		id: 8,
+		title: 'MongoDB',
+		level: 'basic',
+		icon: 'logo/mongodb.png',
+		type: 'database',
+	},
+	{
+		id: 9,
+		title: 'Sequelize',
+		level: 'basic',
+		icon: 'logo/sequelize.png',
+		type: 'database',
+	},
+	{
+		id: 10,
+		title: 'Prisma',
+		level: 'average',
+		icon: 'logo/prisma.png',
+		type: 'database',
+	},
+	{
+		id: 3,
+		title: 'Typescript',
+		level: 'average',
+		icon: 'logo/typescript.png',
+		type: 'language',
+	},
+	{
+		id: 4,
+		title: 'JS',
+		level: 'average',
+		icon: 'logo/js.png',
+		type: 'language',
+	},
+	{
+		id: 5,
+		title: 'PHP',
+		level: 'average',
+		icon: 'logo/php.png',
+		type: 'language',
+	},
+];
+
+/*
+- basic
+- average
+- advanced
+*/
--- src/components/List/SkillsLevelList.tsx
+++ src/components/List/SkillsLevelList.tsx
@@ -1,38 +1,93 @@
+import { skills } from '../../../shared/constant/skills';
+
 const SkillsLevelList = () => {
 	return (
 		<div className='mt-10 p-2'>
 			<div className='grid lg:grid-cols-2 gap-4'>
-				<div className='bg-gray-950 rounded-xl p-4 flex flex-col gap-4'>
-					<h1 className='text-white text-2xl'>Продвинутый</h1>
-					<div className='flex flex-row gap-4'>
-						<div
-							className='bg-white w-[100px] h-[110px] 
-                        flex flex-col items-center p-3 rounded-xl
-                        shadow-xl/15 shadow-white hover:shadow-xl/30 
-                        transform transition-all duration-300 hover:-translate-y-1'
-						>
-							<img width={80} height={80} src='logo/react.png' alt='react' />
-							<b>React</b>
-						</div>
-						<div
-							className='bg-white w-[100px] h-[110px] 
-                        flex flex-col items-center p-3 rounded-xl
-                        shadow-xl/15 shadow-white hover:shadow-xl/30 
-                        transform transition-all duration-300 hover:-translate-y-1'
-						>
-							<img width={80} height={80} src='logo/react.png' alt='react' />
-							<b>React</b>
+				{skills.find((skill) => skill.level === 'advanced') && (
+					<div className='bg-gray-950 rounded-xl p-4 flex flex-col gap-4'>
+						<h1 className='text-white text-2xl'>Продвинутый</h1>
+						<div className='flex flex-row gap-4 flex-wrap'>
+							{skills.map((skill) => {
+								return (
+									skill.level === 'advanced' && (
+										<div
+											key={skill.id}
+											className='bg-white w-[100px] h-[110px] 
+						flex flex-col items-center p-3 rounded-xl
+						shadow-xl/15 shadow-white hover:shadow-xl/30 
+						transform transition-all duration-300 hover:-translate-y-1'
+										>
+											<img
+												width={80}
+												height={80}
+												src={skill.icon}
+												alt={skill.title}
+											/>
+											<b>{skill.title}</b>
+										</div>
+									)
+								);
+							})}
 						</div>
 					</div>
-				</div>
-				<div className='bg-gray-950 rounded-xl p-4'>
-					<h1 className='text-white text-2xl'>Средний</h1>
-					<div className='text-white'>1234</div>
-				</div>
-				<div className='bg-gray-950 rounded-xl p-4'>
-					<h1 className='text-white text-2xl'>Базовый</h1>
-					<div className='text-white'>1234</div>
-				</div>
+				)}
+				{skills.find((skill) => skill.level === 'average') && (
+					<div className='bg-gray-950 rounded-xl p-4 flex flex-col gap-4'>
+						<h1 className='text-white text-2xl'>Средний</h1>
+						<div className='flex flex-row gap-4'>
+							{skills.map((skill) => {
+								return (
+									skill.level === 'average' && (
+										<div
+											key={skill.id}
+											className='bg-white w-[100px] h-[110px] 
+						flex flex-col items-center p-3 rounded-xl
+						shadow-xl/15 shadow-white hover:shadow-xl/30 
+						transform transition-all duration-300 hover:-translate-y-1'
+										>
+											<img
+												width={80}
+												height={80}
+												src={skill.icon}
+												alt={skill.title}
+											/>
+											<b>{skill.title}</b>
+										</div>
+									)
+								);
+							})}
+						</div>
+					</div>
+				)}
+				{skills.find((skill) => skill.level === 'basic') && (
+					<div className='bg-gray-950 rounded-xl p-4'>
+						<h1 className='text-white text-2xl'>Базовый</h1>
+						<div className='flex flex-row gap-4'>
+							{skills.map((skill) => {
+								return (
+									skill.level === 'basic' && (
+										<div
+											key={skill.id}
+											className='bg-white w-[100px] h-[110px] 
+						flex flex-col items-center p-3 rounded-xl
+						shadow-xl/15 shadow-white hover:shadow-xl/30 
+						transform transition-all duration-300 hover:-translate-y-1'
+										>
+											<img
+												width={80}
+												height={80}
+												src={skill.icon}
+												alt={skill.title}
+											/>
+											<b>{skill.title}</b>
+										</div>
+									)
+								);
+							})}
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
