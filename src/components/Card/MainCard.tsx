5578526bd36bb9388152994af2976449fa080b75
Danil Vereshchagin <diafin.bernuly@mail.ru>
2026-02-28 01:44:06 +0300

    Feat: изменение дизайна главной страницы

--- src/components/Card/MainCard.tsx
+++ src/components/Card/MainCard.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import classes from './MainCard.module.scss';
-import { Link } from '../Link/Link';
+import React, {type ReactNode} from 'react';
+import {cn} from "../../shared/lib/utils.ts";
+import {LiquidGlassCard} from "../ui-layouts/LiquidGlassCard.tsx";
+import {NavLink} from "react-router-dom";
 
 interface Props {
-	title?: string;
-	info?: string;
+	title: string;
+	info: string;
 	url: string;
+    className?: string;
+    icon?: ReactNode;
 }
 
-const MainCard: React.FC<Props> = ({ title, info, url }) => {
-	return (
-		<div className={classes.box}>
-			<span></span>
-			<div className={classes.content}>
-				<h2>{title}</h2>
-				<p>{info}</p>
-				<Link url={url} title='Перейти' />
-			</div>
-		</div>
-	);
+const MainCard: React.FC<Props> = ({ title, info, url, className, icon }) => {
+    return (
+        <LiquidGlassCard
+            shadowIntensity='sm'
+            borderRadius='24px'
+            glowIntensity='none'
+            blurIntensity={"sm"}
+            className={cn('rounded-3xl p-6 text-white bg-white/2 flex flex-col', className)}
+        >
+            <NavLink to={url} className='relative z-30 flex flex-col items-center justify-center h-full'>
+                {icon}
+                <div className='text-6xl font-semibold'>{title}</div>
+                <div className='text-lg mt-2 text-center'>{info}</div>
+            </NavLink>
+        </LiquidGlassCard>
+    );
 };
 
 export default MainCard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,31 @@
 import { cn } from '../shared/lib/utils';
 import MainCard from '../components/Card/MainCard';
 import classes from '../components/Card/MainCard.module.scss';
+import {Album, BrainCircuit, User} from "lucide-react";
 
 const Home = () => {
 	return (
-		<div className={cn(classes.container, 'lg:mt-20 lg:gap-10')}>
-			<MainCard title='Обо мне' info='Контактная информация' url='about' />
+		<div className={'center h-full flex flex-row flex-wrap gap-10 items-center justify-center mt-[20%] lg:mt-[10%] md:mt-[20%] mb-[10%]'}>
 			<MainCard
+                className={cn(classes.stretch, 'w-[90%] max-w-full lg:w-[30%] lg:max-w-[30%] md:w-[45%] md:max-w-[45%]')}
+                title='Обо мне'
+                info='Контактная информация'
+                url='about'
+                icon={<User className={'w-20 h-20 md:w-40 md:h-40'} />}
+            />
+			<MainCard
+                className={cn(classes.stretch, 'w-[90%] max-w-full lg:w-[30%] lg:max-w-[30%] md:w-[45%] md:max-w-[45%]')}
 				title='Навыки'
 				info='Информация о навыках (изученных технологиях)'
 				url='skills'
+                icon={<BrainCircuit className={'w-20 h-20 md:w-40 md:h-40'} />}
 			/>
 			<MainCard
+                className={cn(classes.stretch, 'w-[90%] max-w-full lg:w-[30%] lg:max-w-[30%] md:w-[45%] md:max-w-[45%]')}
 				title='Проекты'
 				info='Информация о проектах и используемых технологиях'
 				url='projects'
+                icon={<Album className={'w-20 h-20 md:w-40 md:h-40'} />}
 			/>
 		</div>
 	);
