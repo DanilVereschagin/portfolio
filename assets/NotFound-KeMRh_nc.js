7a8ba74620efe8b2467df74a64fe439169122e7a
Danil Vereshchagin <diafin.bernuly@mail.ru>
2026-03-16 22:48:36 +0300

    Updates

---
+++ assets/NotFound-KeMRh_nc.js
@@ -0,0 +1,1 @@
+import{q as t}from"./index-DCF0e4rh.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx("h1",{className:"text-white text-4xl",children:"Страница не найдена"})});export{r as default};
--- assets/NotFound-U5N3Ua5d.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CnK1O_tQ.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx("h1",{className:"text-white text-4xl",children:"Страница не найдена"})});export{r as default};
